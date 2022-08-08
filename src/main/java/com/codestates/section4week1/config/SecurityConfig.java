75ed8f06f1b2d6368fbbef18e4fcc21134c2a07f
yjy <upgrdrqnu@gmail.com>
2022-08-08 14:42:46 +0900

    to

--- src/main/java/com/codestates/section4week1/config/SecurityConfig.java
+++ src/main/java/com/codestates/section4week1/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.codestates.section4week1.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // (1)
 public class SecurityConfig {
 
 //    @Autowired
@@ -37,3 +39,31 @@ public class SecurityConfig {
     }
 
 }
+
+/*
+(1) SpringSecurity는 Web기반의 Security 외에 Method Security 기능을 제공. 서비스 계층을 직접 호출할때 사용할 수 있는 보안 기능.
+@EnableGlobalMethodSecurity는 MethodSecurity용 설정이 따로 할 때 선언하는 애너테이션.
+
+속성에는 securedEnabled, prePostEnabled, jsr250Enabled 3개의 옵션이 존재.
+1.securedEnabled : @Secured 애노테이션을 사용하여 인가 처리를 하고 싶을때 사용하는 옵션이다.
+기본값은 false
+2.prePostEnabled : @PreAuthorize, @PostAuthorize 애노테이션을 사용하여 인가 처리를 하고 싶을때 사용하는 옵션이다.
+기본값은 false
+3.jsr250Enabled : @RolesAllowed 애노테이션을 사용하여 인가 처리를 하고 싶을때 사용하는 옵션이다.
+기본값은 false
+
+@Secured, @RolesAllowed
+특정 메서드 호출 이전에 권한을 확인한다.
+SpEL 지원하지 않는다.
+@Secured 는 스프링에서 지원하는 애노테이션이며, @RolesAllowed는 자바 표준
+
+@PreAuthorize, @PostAuthorize
+특정 메서드 호출 전, 후 이전에 권한을 확인한다.
+SpEL을 지원한다.
+스프링에서 지원하는 애노테이션이다.
+PostAuthorize는 해당 메서드의 리턴값을 returnObject 로 참조하여 SpEL을 통해 인가 처리를 할 수 있다.
+
+(1)번에서 securedEnabled = true, prePostEnabled = true로 속성을 설정했으므로
+IndexController.java에 새로운 메서드와 @Secured와 @preAuthorize를 추가
+
+ */
--- src/main/java/com/codestates/section4week1/controller/IndexController.java
+++ src/main/java/com/codestates/section4week1/controller/IndexController.java
@@ -4,6 +4,8 @@ import com.codestates.section4week1.model.Member;
 import com.codestates.section4week1.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -60,4 +62,26 @@ public class IndexController {
 
         return "redirect:/login";
     }
+
+    @Secured("ROLE_ADMIN") // 1개의 권한을 주고 싶을 때 사용
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "info";
+    } // (1)
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") // 1개 이상의 권한을 주고 싶을 때 사용. 메서드가 실행되고 응답하기 직전에 권한을 검사하는데 사용.
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "data";
+    } // (2)
 }
+
+/*
+(1) 1개의 권한을 주고 싶을 때 사용
+SecurityConfig에 .antMatchers("/info/**").access("hasRole('ROLE_ADMIN')") 코드를 추가하는 것과 같은 동작.
+
+(2) 1개 이상의 권한을 주고 싶을 때 사용.
+SecurityConfig에 .antMatchers("/data/**").access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") 코드를 추가하는 것과 같은 동작.
+#을 사용하면 파라미터에 접근할 수 있다 : (ex) @PreAuthorize("isAuthenticated() and (( #user.name == principal.name ) or hasRole('ROLE_ADMIN'))")
+클라이언트에 응답하기 전에 로그인 상태 또는 반환되는 사용자 이름과 현재 사용자 이름에 대한 검사, 현재 사용자가 관리자 권한을 가지고 있는지 등의 권한 후처리를 할 수 있다.
+ */
